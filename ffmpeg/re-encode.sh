e9e763a2c8691e2f2e63759b01e33104f3765c9f
Vortex Acherontic <vortex@z-ray.de>
2022-09-29 17:35:32 +0200

    ffmpeg NVENC re-encode 6K

---
+++ ffmpeg/re-encode.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+##################################################
+# Converts all files in a directory using ffmpeg
+# With a 6000k bitrate (Default Twtich sStream quality)
+# using NVENC.
+
+mkdir -p $PWD/converted
+for f in *; do
+	if [ -f "$f" ]; then
+		echo "Processing $f ..."
+		ffmpeg -threads 1 \
+        -hwaccel nvdec \
+        -hwaccel_device 0 \
+        -hwaccel_output_format cuda \
+        -i "$f" \
+        -c:v h264_nvenc \
+        -gpu:v 0 \
+        -cq:v 21 \
+        -rc:v vbr \
+        -tune:v ll \
+        -preset:v p1 \
+        -b:v 0 \
+        -maxrate:v 6000k \
+        -bufsize:v 6000K \
+        -c:a aac \
+        -b:a 160k \
+        -movflags +faststart \
+        -f mp4 "$PWD/converted/${f%.*}.mp4"
+	fi
+done 
