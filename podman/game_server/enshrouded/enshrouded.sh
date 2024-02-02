32de8b38f19ade52fb6a1b08908465828c184102
Imo 'Vortex Acherontic' Hester <vortex@z-ray.de>
2024-02-03 00:23:33 +0100

    Add enshrouded dedicated server

---
+++ podman/game_server/enshrouded/enshrouded.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+container_name=enshrouded_server
+
+mkdir -p $PWD/game
+chmod -R 777 $PWD/game
+chmod 777 entrypoint.sh
+
+podman build . -t ${container_name}
+
+podman run -di \
+-p 15637:15637/udp \
+-p 15636:15636/udp \
+--restart unless-stopped \
+-v $PWD/game:/home/user/Steam:z \
+--name $container_name \
+${container_name}
---
+++ podman/game_server/enshrouded/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -ex
+
+[ "$UID" != 0 ] || {
+    mkdir -p ~user/Steam
+    chown user: ~user/Steam
+    runuser -u user "$0" "$@"
+    exit 0
+}
+
+GAMEDIR="$HOME/Steam/steamapps/common/EnshroudedServer"
+
+cd "$HOME"
+STEAMCMD="./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous $STEAMCMD"
+[ -z "$BETA" ] || STEAMCMD="$STEAMCMD -beta experimental"
+
+# eval to support quotes in $STEAMCMD
+eval "$STEAMCMD +app_update 2278520 +quit"
+
+mkdir -p "$GAMEDIR/Logs"
+
+#rm -f /tmp/.X1-lock
+#Xvfb :1 -screen 0 800x600x24 &
+#export WINEDLLOVERRIDES="mscoree,mshtml="
+#export DISPLAY=:1
+
+cd "$GAMEDIR"
+
+[ "$1" = "bash" ] && exec "$@"
+
+wine ./enshrouded_server.exe "$@" &> Logs/wine.log
