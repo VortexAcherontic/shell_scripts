e006544797c6ee733b2a1f8725887e4fce956efe
Imo 'Vortex Acherontic' Hester <vortex@z-ray.de>
2024-02-07 20:50:12 +0100

    Add Steam CMD preset server

---
+++ podman/game_server/steamcmd_preset_server/steam_cmd_server.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+container_name=steam_cmd_server
+
+mkdir -p $PWD/game
+chmod -R 777 $PWD/game
+chmod 777 entrypoint.sh
+
+podman build . -t ${container_name}
+
+podman run -di \
+-p 15637:15637/udp \
+-p 15637:15637/tcp \
+-p 15636:15636/udp \
+-p 15636:15636/tcp \
+--restart unless-stopped \
+-v $PWD/game:/home/user/Steam:z \
+--name $container_name \
+${container_name}
