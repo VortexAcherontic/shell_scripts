448e0cb190dfc8600da9c6d7697802353187870b
Imo 'Vortex Acherontic' Hester <vortex@z-ray.de>
2024-02-07 20:50:46 +0100

    Update enshrouded server

--- podman/game_server/enshrouded/entrypoint.sh
+++ podman/game_server/enshrouded/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+STEAMAPPID=2278520
 
 [ "$UID" != 0 ] || {
     mkdir -p ~user/Steam
@@ -10,18 +11,12 @@
 GAMEDIR="$HOME/Steam/steamapps/common/EnshroudedServer"
 
 cd "$HOME"
-STEAMCMD="./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous $STEAMCMD"
-[ -z "$BETA" ] || STEAMCMD="$STEAMCMD -beta experimental"
+STEAMCMD="./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous"
 
 # eval to support quotes in $STEAMCMD
-eval "$STEAMCMD +app_update 2278520 +quit"
+eval "$STEAMCMD +app_update $STEAMAPPID +quit"
 
 mkdir -p "$GAMEDIR/Logs"
-
-#rm -f /tmp/.X1-lock
-#Xvfb :1 -screen 0 800x600x24 &
-#export WINEDLLOVERRIDES="mscoree,mshtml="
-#export DISPLAY=:1
 
 cd "$GAMEDIR"
 
