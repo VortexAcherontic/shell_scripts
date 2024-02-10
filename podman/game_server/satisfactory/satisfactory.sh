cf45a18d0ff5e4ca262232057d14a0612a163815
Imo 'Vortex Acherontic' Hester <vortex@z-ray.de>
2024-02-10 14:43:53 +0100

    Update Satisfactory Server

--- podman/game_server/satisfactory/satisfactory.sh
+++ podman/game_server/satisfactory/satisfactory.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
+container_name=satisfactory_server
 mkdir -p $PWD/game/config
 mkdir -p $PWD/game/steam
 chmod -R 777 $PWD/game
 
+podman pull wolveix/satisfactory-server:dev
+
 podman run -d \
---name=satisfactory_server \
+--name=${container_name} \
 -h satisfactory-server \
 -e MAXPLAYERS=4 \
 -e PGID=1000 \
@@ -16,4 +19,5 @@ podman run -d \
 -p 7777:7777/udp \
 -p 15000:15000/udp \
 -p 15777:15777/udp \
+--replace \
 wolveix/satisfactory-server:dev
