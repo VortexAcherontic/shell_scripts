52c87522ebf1019dbe4dc3bfc238ed00e33e0a5d
Vortex Acherontic <vortex@z-ray.de>
2024-06-23 23:29:16 +0200

    Add to_pro_res script
    Converts any video footage to Apple ProRes using ffmpeg. Make sure your instalaltion of ffmpeg supports "prores_ks"

---
+++ ffmpeg/to_pro_res.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+#################################################################
+# This script converts all files with a file extention          #
+# contained in the formats array to Apple ProRes using ffmpeg.  #
+# Note: Make sure you have a version of ffmpeg installed        #
+# with support for ffmpeg.                                      #
+# Verfy using: ffmpeg -encoders | grep -i "prores_ks"           #
+#################################################################
+
+mkdir -p "$PWD/converted"
+declare -a formats=("*.mkv" "*.mp4" "*.avi")
+
+to_pro_res() {
+	absolute_file="$PWD/$1"
+	echo "Processing $absolute_file ..."
+	ffmpeg -i "$absolute_file" \
+	-c:v prores_ks \
+	-profile:v 4 \
+	-vendor apl0 \
+	-bits_per_mb 8000 \
+	-pix_fmt yuva444p10le \
+	"$PWD/converted/${1%.*}.mov"
+	echo "Done $absolute_file"
+}
+
+for format in "${formats[@]}"; do
+	for file in $format; do
+		if [ -f "$file" ]; then
+			to_pro_res "$file"
+		fi
+	done
+done
