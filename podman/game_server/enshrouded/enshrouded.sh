4db6e3fd50329dfedf63a64cf8b4632e7acd1e25
Vortex Acherontic <vortex@z-ray.de>
2024-08-20 11:19:59 +0200

    Update enshrouded.sh

--- podman/game_server/enshrouded/enshrouded.sh
+++ podman/game_server/enshrouded/enshrouded.sh
@@ -3,16 +3,18 @@ container_name=enshrouded_server
 
 mkdir -p $PWD/game
 chmod -R 777 $PWD/game
-chmod 777 entrypoint.sh
-
-podman build . -t ${container_name}
 
 podman run -di \
 -p 15637:15637/udp \
 -p 15637:15637/tcp \
 -p 15636:15636/udp \
 -p 15636:15636/tcp \
+-e SERVER_NAME="Enshrouded Docker Server" \
+-e UPDATE_CRON="*/30 * * * *" \
+-e PUID=4711 \
+-e PGID=4711 \
+-e SERVER_IP="0.0.0.0" \
 --restart unless-stopped \
--v $PWD/game:/home/user/Steam:z \
+-v $PWD/game:/opt/enshrouded:z \
 --name $container_name \
-${container_name}
+mornedhels/enshrouded-server
