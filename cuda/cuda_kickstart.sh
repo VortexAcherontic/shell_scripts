1aeaff196276a34e9b44317d6275bfebd2cdc263
Vortex Acherontic <vortex@z-ray.de>
2025-11-10 19:56:52 +0100

    Update CUDA kickstart

--- cuda/cuda_kickstart.sh
+++ cuda/cuda_kickstart.sh
@@ -4,7 +4,12 @@
 # next to blender downloaded from #
 # blender.org                     #
 ###################################
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo $SCRIPT_DIR
-$SCRIPT_DIR/blender -b $SCRIPT_DIR/empty.blend -f 20 -- --cycles-device CUDA --cycles-print-stats
+driver_version=$(nvidia-smi --query-gpu=driver_version | tail -n +2)
+if [ $driver_version == "580.105.08"  ]; then
+    echo $(nvidia-smi)
+else
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    echo $SCRIPT_DIR
+    $SCRIPT_DIR/blender -b $SCRIPT_DIR/empty.blend -f 20 -- --cycles-device CUDA --cycles-print-stats
+fi;
 # Workarounds: https://forums.developer.nvidia.com/t/unrelaibale-cuinit-with-sandboxes-and-containers-cuda-cuinit-unknown-error/333562/1
